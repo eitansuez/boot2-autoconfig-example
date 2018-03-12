b4929af8e54c61b73bf4492ef8abba2fd3edcf3b
Eitan Suez <esuez@pivotal.io>
2018-03-11 23:01:07 -0500

    step 2: adding a hello service bean to application context (in hello-app), application runs now

--- hello-app/src/main/java/com/example/HelloApplication.java
+++ hello-app/src/main/java/com/example/HelloApplication.java
@@ -2,11 +2,17 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HelloApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(HelloApplication.class, args);
+  }
+
+  @Bean
+  HelloService helloService() {
+    return new ConsoleHelloService("Hello", "!");
   }
 }
