aa347ece398b219f4f575c82be445d7b45e44658
Eitan Suez <esuez@pivotal.io>
2018-03-11 23:11:52 -0500

    step 4: contributing bean only if missing, using @Conditional annotations

--- hello-starter/src/main/java/com/example/HelloAutoConfig.java
+++ hello-starter/src/main/java/com/example/HelloAutoConfig.java
@@ -1,11 +1,15 @@
 package com.example;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@ConditionalOnClass(HelloService.class)
 @Configuration
 public class HelloAutoConfig {
 
+  @ConditionalOnMissingBean
   @Bean
   HelloService helloService() {
     return new ConsoleHelloService("Bonjour", ".");
